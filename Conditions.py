5a660c4ef3c3e7b4b31a660f73af174569fa69c7
Rheena Catherine <rheenahmacharia95@gmail.com>
2021-02-08 08:43:43 +0300

    Add files via upload
    Conditions in python

---
+++ Conditions.py
@@ -0,0 +1,27 @@
+#If statement - it checks for a condition in a statement. If the condition returns true, the code after the statement is executed otherwise, it will just be skipped
+x = input('First number: ')
+y = input('Second number: ')
+
+x = int(x)
+y = int(y)
+
+if x < y: #colon opens an if block
+    print('x is less than y!')
+elif x > y:
+    print('x is greater than y!')
+else:
+    print('x is equal to y!')
+
+#nested if statements - this are chains of if statements
+if x < y:
+    print('x is less than y!')
+    if x >= 0:
+        print('x is positive!')
+    else:
+        print('x is negative!')
+elif x > y:
+    print('x is greater than y!')
+    if x > 100:
+        print('x is greater than 100!')
+else:
+    print('x is equal to y!')
