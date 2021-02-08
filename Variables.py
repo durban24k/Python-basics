84a38ec13c00d1cf883b14a89f5b2c62e73a622e
Rheena Catherine <rheenahmacharia95@gmail.com>
2021-02-08 08:36:39 +0300

    Add files via upload
    Basics on variables in python

---
+++ Variables.py
@@ -0,0 +1,32 @@
+print("Hello World!")
+
+#Different variables include;
+#Integer: This represents a whole number
+int 10 9 8
+
+#String: Can use single/double quotation marks or triple quotation marks for a multi-line string
+str 'Hello' "Hello"
+'''
+This is a Python fundamentals class.
+It teaches on variables and data types!
+'''
+
+#Float: Represents a decimal number
+float 10.9, 8.456
+
+#Complex:
+complex (10, 5)
+
+#Boolean: One of two values, either true or false. Used in conditions
+bool True False
+
+#Type function: Tells you what type of variable you're working with
+print(type (10))
+
+#Defining variables
+x = 10
+y = "Hello!"
+Whatever = True
+
+print(x)
+
