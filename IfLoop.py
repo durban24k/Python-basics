3f66087b34e3f613fe6bd754bf7ac87672f2fed8
Rheena Catherine <rheenahmacharia95@gmail.com>
2021-02-08 08:44:09 +0300

    Add files via upload
    If loop in python

---
+++ IfLoop.py
@@ -0,0 +1,8 @@
+#While loop - It executes a certain piece of code over and over again as long as a certain condion is met
+x = 0
+
+while x < 10:
+    x += 1 #increases x by 1 with every iterartion
+    print(x)
+
+#To print an endless loop use while true:
