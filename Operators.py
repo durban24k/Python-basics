341800cf33507418edda9f2ad386fdbee8527037
Rheena Catherine <rheenahmacharia95@gmail.com>
2021-02-08 08:42:12 +0300

    Add files via upload
    Operators in python

---
+++ Operators.py
@@ -0,0 +1,65 @@
+#Arithmetic operators: Used for calculation + - * /
+print(8 + 5)
+print(12 - 3)
+print(55 * 4)
+print(30 / 4)
+
+#Modulus %: Gives the reminder after division
+print(10 % 3)
+
+#Exponent **: Getting the power of a number
+print(2 ** 5)
+
+#Floor division //: It divides 2 numbers and rounds down the result
+print(10 // 8)
+
+#Assignment operator =: Assigns the value on the right to the value on the left
+x = 10
+
+#Assignment operator +=: Adds the value to the variable
+x += 10
+
+#Assignment operator -=: Subtracts the value to the variable
+x -= 10
+
+#Assignment operator *=: Multiplies the value to the variable
+x *= 10
+
+#Assignment operator -=: Divides the value to the variable
+x /= 10
+
+#Comparison operators: Always returns a boolean == != < > <= >=
+x = 10
+y = 20
+
+print( x == y)
+print( x != y)
+print( x < y)
+print( x > y)
+print( x <= y)
+print( x >= y)
+
+#Logical operators - and or not: They are used to logically connect comparisons
+#And demands both sides to be true
+10 < 20 and 20 < 30
+
+#Or demands that atleast one side has to be true
+10 < 20 or 20 > 30
+
+#Not is negation
+print(not True)
+
+#Membership operators
+
+#Bitwise operators
+
+#User input - input() enters the value the user inputs
+x = input("Enter a number: ")
+y = input("Enter your name: ")
+
+#Typecast: This is pre-entering what value the variable is to be input by the user
+x = int(x)
+y = str(y)
+
+print(x)
+print(y)
